a9fbdb4f2b69d3a1dfef212a918dcba08b53dfc6
Manuel Barros Reyes <manuca@gmail.com>
2014-12-26 00:39:41 -0300

    files distributed in dirs

--- lib/project_tracker.rb
+++ lib/project_tracker.rb
@@ -1,10 +1,11 @@
 require "project_tracker/version"
 
 require 'project_tracker/entity'
-require 'project_tracker/client'
-require 'project_tracker/project'
-require 'project_tracker/task'
-require 'project_tracker/worker'
+
+require 'project_tracker/domain/client'
+require 'project_tracker/domain/project'
+require 'project_tracker/domain/task'
+require 'project_tracker/domain/worker'
 
 require 'project_tracker/repository'
 require 'project_tracker/repository/projects/memory'
