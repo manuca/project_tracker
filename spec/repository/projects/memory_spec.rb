2525b932c0956a2d0b50bd2429e13def5c741ed5
Manuel Barros Reyes <manuca@gmail.com>
2015-01-18 16:37:44 -0300

    common repository examples on separate file

--- spec/repository/projects/memory_spec.rb
+++ spec/repository/projects/memory_spec.rb
@@ -1,51 +1,12 @@
 require 'spec_helper'
+require 'support/common_repository_examples'
 
 module ProjectTracker
   module Repository
     module Projects
-      # This will be a common spec for all repository backends
       describe Memory do
-        let(:tennant)    { Tennant.new("A tennant") }
-        let(:client)     { Client.new(tennant, "Some client") }
-        let(:project)    { Project.new(tennant, "Project name", client) }
         let(:repository) { Memory.new }
-
-        describe "#save" do
-          it "saves an instance" do
-            repository.save(project)
-            expect(project.id).not_to be_nil
-            expect(project.repository).to eq(repository)
-          end
-        end
-
-        describe "#find" do
-          it "retrieves project by id" do
-            repository.save(project)
-            retrieved = repository.find(project.id)
-            expect(retrieved).to eq(project)
-          end
-        end
-
-        describe "when adding task to a project" do
-          let(:task_date) { Date.today }
-
-          let(:worker)     { Worker.new(tennant, "Dude working") }
-
-          before do
-            repository.save(project)
-            repository.add_task(project, Task.new(worker, task_date, "12:00", "13:00", "a task"))
-          end
-
-          it "#tasks contains the stored task" do
-            tasks = repository.tasks(project)
-            expect(tasks.count).to eq(1)
-          end
-
-          it "#remove_task removes the task" do
-            repository.remove_task(project, Task.new(worker, task_date, "12:00", "13:00", "a task"))
-            expect(repository.tasks(project).count).to eq(0)
-          end
-        end
+        it_behaves_like "a repository"
       end
     end
   end
---
+++ spec/support/common_repository_examples.rb
@@ -0,0 +1,43 @@
+module ProjectTracker
+  shared_examples "a repository" do
+    let(:tennant) { Tennant.new("A tennant") }
+    let(:client)  { Client.new(tennant, "Some client") }
+    let(:project) { Project.new(tennant, "Project name", client) }
+
+    describe "#save" do
+      it "saves an instance" do
+        repository.save(project)
+        expect(project.id).not_to be_nil
+        expect(project.repository).to eq(repository)
+      end
+    end
+
+    describe "#find" do
+      it "retrieves project by id" do
+        repository.save(project)
+        retrieved = repository.find(project.id)
+        expect(retrieved).to eq(project)
+      end
+    end
+
+    describe "when adding task to a project" do
+      let(:task_date) { Date.today }
+      let(:worker)    { Worker.new(tennant, "Dude working") }
+
+      before do
+        repository.save(project)
+        repository.add_task(project, Task.new(worker, task_date, "12:00", "13:00", "a task"))
+      end
+
+      it "#tasks contains the stored task" do
+        tasks = repository.tasks(project)
+        expect(tasks.count).to eq(1)
+      end
+
+      it "#remove_task removes the task" do
+        repository.remove_task(project, Task.new(worker, task_date, "12:00", "13:00", "a task"))
+        expect(repository.tasks(project).count).to eq(0)
+      end
+    end
+  end
+end
